82fa30cc4a30d924c0ba117c2c2bddc242efebe5
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-08-25 14:09:38 +0200

    chore: Update Prettier configuration to use single quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,28 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
+// export default [
+//   {languageOptions: { globals: globals.browser }},
+//   pluginJs.configs.recommended,
+// ];
+
+// import globals from 'globals'
+// import pluginJs from '@eslint/js'
+
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      //Own rules added here
+    },
+  },
   pluginJs.configs.recommended,
-];
+]
