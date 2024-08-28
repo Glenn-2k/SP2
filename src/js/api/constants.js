0eb17b34218a0a9d36e082c3b115dfb9daaa07f4
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-08-28 13:41:39 +0200

    chore: Add API constants for authentication and auctions

---
+++ src/js/api/constants.js
@@ -0,0 +1,7 @@
+export const API_KEY = '7d024c34-3155-4587-a0e9-b29be6b11556';
+
+export const API_BASE = 'https://v2.api.noroff.dev';
+export const API_AUTH = '/auth';
+export const API_LOGIN = '/login';
+export const API_REGISTER = '/register';
+export const API_AUCTIONS = '/auction';
