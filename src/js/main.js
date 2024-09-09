3324c4b654ffd86bae91472e7f342f54eded64d8
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-09-09 10:46:30 +0200

    added router and made it working

--- src/js/main.js
+++ src/js/main.js
@@ -8,12 +8,5 @@ import * as bootstrap from 'bootstrap';
 // Import custom JS
 
 import router from './router';
-import { getListings } from './api/listings/getListings.js';
-import { renderListings } from './renders/renderListings.js';
-// import { getListing } from './api/listings/getListing.js';
 
 router();
-
-getListings();
-renderListings();
-// getListing();
--- src/js/router.js
+++ src/js/router.js
@@ -1,3 +1,22 @@
+import { getListings } from './api/listings/getListings';
+import { renderListings } from './renders/renderListings';
+
 export default function router() {
   console.log('router.js');
+
+  const path = location.pathname;
+
+  if (path === '/' || path === '/index.html') {
+    console.log('Home page');
+  } else if (path === '/profile/index.html' || path === '/profile') {
+    console.log('Profile page');
+  } else if (path === '/lisings/index.html' || path === '/listings') {
+    console.log('Listings page');
+    getListings();
+    renderListings();
+  }
+
+  getListings();
+  renderListings();
+  // getListing();
 }
