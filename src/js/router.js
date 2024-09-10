93393c0400ddb4be9bc1e574fb9873c8922dfec3
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-09-10 11:47:52 +0200

    chore: Refactor router.js to improve code organization and remove unnecessary console.log statements

--- src/js/router.js
+++ src/js/router.js
@@ -19,12 +19,10 @@ export default function router() {
     loginFormListener();
   } else if (path === '/profile/index.html' || path === '/profile/') {
     loginFormListener();
-
     console.log('Profile page');
   } else if (path === '/listings/index.html' || path === '/listings/') {
     console.log('Listings page');
     loginFormListener();
-
     getListings();
     renderListings();
   }
