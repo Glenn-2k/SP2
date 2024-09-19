7608302d5b260e3375ce06a46cd8153d8be930f7
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-09-19 10:30:06 +0200

    Refactor vite.config.js to add rollupOptions for multiple entry points

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ export default {
   root: resolve(__dirname, 'src'),
   build: {
     outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        profile: resolve(__dirname, 'src/profile/index.html'),
+        listings: resolve(__dirname, 'src/listings/index.html'),
+      },
+    },
   },
   server: {
     port: 8080,
