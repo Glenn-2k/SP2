41189180297bb0d37cee90e237f88c3ddc1e2ec4
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-09-20 17:19:14 +0200

    Refactor getListings API URL for active listings with sorting parameters

--- src/js/api/listings/getListings.js
+++ src/js/api/listings/getListings.js
@@ -1,13 +1,16 @@
 import { API_BASE, API_AUCTIONS } from '../constants';
 
 const listings = '/listings';
+const isActive = '?_active=true';
+const sortOrder = 'desc';
+const sortBy = 'created';
 
 export async function getListings() {
-  const url = `${API_BASE}${API_AUCTIONS}${listings}`;
-
+  const url = `${API_BASE}${API_AUCTIONS}${listings}${isActive}&sort=${sortBy}&sortOrder=${sortOrder}`;
+  console.log(url);
   const response = await fetch(url);
   const results = await response.json();
-
   console.log(results);
+
   return results.data;
 }
