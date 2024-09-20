fb72d4736d63944067ea7b83f7309c01d688393d
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-09-20 13:22:31 +0200

    adding loaders to fetching and rendering

--- src/js/renders/renderListings.js
+++ src/js/renders/renderListings.js
@@ -1,17 +1,26 @@
 import { getListings } from '../api/listings/getListings';
 import { listingTemplate } from '../templates/listingTemplate';
 
+const loader = document.getElementById('loader');
+
 export async function renderListings() {
+  loader.style.display = 'block';
   const listingsContainer = document.getElementById('listingsContainer');
   listingsContainer.innerHTML = '';
 
   try {
     const listings = await getListings();
+
+    loader.style.display = 'none';
     listings.forEach((data) => {
       const listingCard = listingTemplate(data);
       listingsContainer.append(listingCard);
     });
   } catch (error) {
     console.error(error);
+
+    loader.style.display = 'none';
+    listingsContainer.innerHTML =
+      '<p>Error loading listings. Please try again later.</p>';
   }
 }
