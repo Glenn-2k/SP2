94b46a3aeb5e942f46a4473c6f0776f3d20f6a6d
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-09-21 17:54:35 +0200

    Refactor getListings API to use dynamic sorting and filtering parameters

--- src/js/api/listings/getListings.js
+++ src/js/api/listings/getListings.js
@@ -2,8 +2,8 @@ import { API_BASE, API_AUCTIONS } from '../constants';
 
 const listings = '/listings';
 const isActive = '?_active=true';
-// const sortOrder = 'desc';
-// const sortBy = 'created';
+const sortOrder = 'desc';
+const sortBy = 'created';
 
 /**
  * Fetches a list of auction listings, sorted and filtered by the provided parameters.
@@ -15,7 +15,7 @@ const isActive = '?_active=true';
  */
 
 export async function getListings() {
-  const url = `${API_BASE}${API_AUCTIONS}${listings}${isActive}&sort=created&sortOrder=desc`;
+  const url = `${API_BASE}${API_AUCTIONS}${listings}${isActive}&sort=${sortBy}&sortOrder=${sortOrder}`;
   console.log(url);
   const response = await fetch(url);
   const results = await response.json();
