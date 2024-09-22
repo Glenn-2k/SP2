79a511381f8cb26d175b6fb4615cb5eca788b47d
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-09-22 20:50:40 +0200

    Refactor profileTemplate.js to update profile avatar image source

--- src/js/templates/profileTemplate.js
+++ src/js/templates/profileTemplate.js
@@ -47,7 +47,7 @@ export async function profileTemplate(data) {
     data.avatar.url ===
       'https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=400&w=400'
   ) {
-    profileAvatar.src = './images/placeholderProfile.svg';
+    profileAvatar.src = './../images/placeholderProfile.svg';
   }
   profileAvatar.alt = 'Profile Avatar';
 
